d8706235208716b62534cef3bbc621c9330c6faa
Sergo test <abusov.sergei@yandex.ru>
2023-05-20 17:13:06 +0300

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int priceOfBilet = 13676; // Стоимость билета
+        int bonusPerMile = 20; // Количество рублей для одной бонусной мили
+
+        int bonus = priceOfBilet / bonusPerMile; // Количество начисленных бонусов 
+
+        System.out.println("Количество начисленных бонусов:" + " " + bonus);
+
+
+    }
+}
